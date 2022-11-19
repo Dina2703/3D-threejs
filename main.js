57653d4d488abfb11a507790202a6322033384cc
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-11-18 21:59:34 -0500

    Animation Loop and Lightning

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./style.css";
-
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // for 3d app we need 3 objects: scene, camera, renderer
 const scene = new THREE.Scene();
@@ -30,9 +30,8 @@ renderer.render(scene, camera);
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 
 // 2-step Material - the wrapping paper for an object
-const material = new THREE.MeshBasicMaterial({
+const material = new THREE.MeshStandardMaterial({
   color: 0xff6347,
-  wireframe: true,
 });
 
 // 3- step Mesh - geometry + materail
@@ -40,8 +39,25 @@ const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
 
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
 function animate() {
   requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  controls.update();
   renderer.render(scene, camera);
 }
 
